e52d23a22b02d82c68f32efd9be4a4c84fbfd79a
Max <vloxomire@gmail.com>
2018-08-18 20:47:40 -0300

    construccion de los layout

---
+++ app/src/main/java/com/beetle/dragon/sabado18/Interfaz/Activity3Activity.java
@@ -0,0 +1,15 @@
+package com.beetle.dragon.sabado18.Interfaz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.beetle.dragon.sabado18.R;
+
+public class Activity3Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3);
+    }
+}
---
+++ app/src/main/java/com/beetle/dragon/sabado18/Interfaz/Sabado18Activity.java
@@ -0,0 +1,15 @@
+package com.beetle.dragon.sabado18.Interfaz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.beetle.dragon.sabado18.R;
+
+public class Sabado18Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sabado18);
+    }
+}
