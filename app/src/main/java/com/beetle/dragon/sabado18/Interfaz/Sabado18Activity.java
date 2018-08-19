3a7fcd6d9c2054aa3931cb73f5ccad92cc17a5a7
Max <vloxomire@gmail.com>
2018-08-18 21:00:40 -0300

    Instanciado y find
    Instanciado de objetos y vinculados

--- app/src/main/java/com/beetle/dragon/sabado18/Interfaz/Sabado18Activity.java
+++ app/src/main/java/com/beetle/dragon/sabado18/Interfaz/Sabado18Activity.java
@@ -2,14 +2,24 @@ package com.beetle.dragon.sabado18.Interfaz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.beetle.dragon.sabado18.R;
 
 public class Sabado18Activity extends Activity {
-
+    private EditText et1,et2;
+    private Button suma, resta, division, multiplicacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sabado18);
+
+    et1=findViewById(R.id.et1);
+    et2=findViewById(R.id.et2);
+    suma=findViewById(R.id.suma);
+    resta=findViewById(R.id.resta);
+    division=findViewById(R.id.division);
+    multiplicacion=findViewById(R.id.multiplicacion);
     }
 }
