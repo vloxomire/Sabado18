41ad3f52c91be3a9c70c10b10a3452f2393ca197
Max <vloxomire@gmail.com>
2018-08-19 23:20:26 -0300

    creo el dao de calculadora

--- app/src/main/java/com/beetle/dragon/sabado18/DAO/DaoCalculadora.java
+++ app/src/main/java/com/beetle/dragon/sabado18/DAO/DaoCalculadora.java
@@ -1,8 +1,18 @@
 package com.beetle.dragon.sabado18.DAO;
 
+import com.beetle.dragon.sabado18.Models.Calculadora;
+
 import java.util.ArrayList;
 
 public class DaoCalculadora {
     ArrayList<Calculadora> calculadoraArrayList = new ArrayList<>();
-    public void addCalculadora
+    public void addCalculadora(Calculadora unaCalculadora){
+        calculadoraArrayList.add(unaCalculadora);
+    }
+    public Integer getSizeArrayCalculadorea(){
+        return calculadoraArrayList.size();
+    }
+    public Calculadora getCalculadora(Integer i){
+        return calculadoraArrayList.get(i);
+    }
 }
